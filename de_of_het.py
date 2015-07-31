90b31808a31846da4f18d1945d97817e89faf238
Aagje <amwcvandermeer@gmail.com>
2015-07-31 16:52:39 -0400

    Update de_of_het.py

--- de_of_het.py
+++ de_of_het.py
@@ -7,12 +7,14 @@ def de_or_het(noun):
 		het_woorden = f.read().split('\r')
 
 	het_woorden = [w.lower() for w in het_woorden]	
-
+	print het_woorden[:20]
+	
 	# and we have an excel file that contains dubious words
 	with open('ambiguous_nouns', 'r') as f:
 		ambiguous_nouns = f.read().split('\r')
-
+	
 	ambiguous_nouns = [w.lower() for w in ambiguous_nouns]	
+	print ambiguous_nouns
 	message = """\nYour noun may be either a 'de' or a 'het' word. Chosing one or the
 other may change the meaning of '{0}'. When in doubt, advice a dictionary.
 	"""
